e9e9830b096f263468034976404507791bbc0389
Ermakrm <ermakrm@yandex.ru>
2023-08-03 19:20:55 +0300

    Add comments

--- main-service/src/main/java/ru/practicum/comment/mapper/CommentMapper.java
+++ main-service/src/main/java/ru/practicum/comment/mapper/CommentMapper.java
@@ -3,6 +3,7 @@ package ru.practicum.comment.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
 import ru.practicum.comment.dto.CommentRequestDto;
 import ru.practicum.comment.dto.CommentResponseDto;
 import ru.practicum.comment.model.Comment;
@@ -10,9 +11,11 @@ import ru.practicum.comment.model.Comment;
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
 @Mapper(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
         componentModel = SPRING)
 public interface CommentMapper {
     Comment toComment(CommentRequestDto commentRequestDto);
+
     @Mapping(source = "author.name", target = "author")
     CommentResponseDto toCommentResponseDto(Comment comment);
 }
