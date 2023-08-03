607d9afe94498c89313fc17e1a399ba855ce844f
Ermakrm <ermakrm@yandex.ru>
2023-08-03 19:25:14 +0300

    Add comments

--- main-service/src/main/java/ru/practicum/comment/repository/CommentRepository.java
+++ main-service/src/main/java/ru/practicum/comment/repository/CommentRepository.java
@@ -10,5 +10,6 @@ import java.util.List;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findAllByAuthorId(Long authorId, Pageable pageable);
+
     List<Comment> findAllByEventId(Long eventId, Pageable pageable);
 }
